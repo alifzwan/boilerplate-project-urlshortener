ac57f4f2ee791d1f1e1761efb3179023f7c03c42
alifzwan <alifzakwan529@gmail.com>
2024-06-17 22:57:29 +0800

    chore: Update server.js to use PORT environment variable

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -19,6 +17,8 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
